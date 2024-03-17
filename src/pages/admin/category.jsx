946b1894c1935765e9fac2f71cc1722a9f582cb3
Aman Verma <amanv5053s@gmail.com>
2024-03-18 05:06:42 +0530

    change to check

--- src/pages/admin/category.jsx
+++ src/pages/admin/category.jsx
@@ -211,8 +211,11 @@ export function Category() {
           <div className="flex flex-wrap -mx-3 mb-5">
             <div className="w-full max-w-full px-3 mb-6  mx-auto">
               <div className="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white">
+
                 <div className="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
-                  <div className="flex-auto block py-8 pt-6 px-9">
+
+                  <div className="flex-auto block py-8 pt-6 px-4">
+
                     <div className="overflow-x-auto">
                       <table className="w-full my-0 align-middle text-dark border-neutral-200">
                         <thead className="align-bottom">
@@ -309,6 +312,7 @@ export function Category() {
                         </tbody>
                       </table>
                     </div>
+
                   </div>
                 </div>
               </div>
